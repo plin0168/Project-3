1187a08e396dece2e138937164fd3332fee244b8
Philip Lin <philipo227@hotmail.com>
2016-10-10 10:20:16 -0700

    setting up Local signup

--- config/passport.js
+++ config/passport.js
@@ -2,3 +2,22 @@ var
   passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   User = require('../models/User.js')
+
+//creating session for cookie, does login
+passport.serializeUser(function(user,done){
+  done(null, user.id)
+})
+
+//takes cookie, translate, find id, find user with id, go to user page
+passport.deserializeUser(function(id,done){
+  User.findById(id, function(err,user){
+    done(err,user)
+  })
+})
+
+//PASSPORT LOCAL Strategy
+
+//local sign-up
+passport.use('local-signup', new LocalStrategy({
+  
+}))
