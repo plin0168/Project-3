9cd3c5a4463f9ca5684a353390bf7a1b586796bb
David Weber <davidjwe33@gmail.com>
2016-10-10 10:31:39 -0700

    created isLoggedIn function and /signup route

---
+++ routes/usersRouter.js
@@ -0,0 +1,22 @@
+//user routes
+var
+  express = require('express'),
+  passport = require('passport'),
+  usersRouter = express.Router(),
+  usersController = require('../controllers/usersController.js'),
+  User = require('../models/User.js')
+
+
+//The root will be the login page
+
+usersRouter.route('/signup')
+  .get(usersController.new)
+  .post(usersController.create)
+  })
+
+  function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) return next()
+  res.redirect('/games')
+}
+
+  module.exports = usersRouter;
