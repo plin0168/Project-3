8e28248e2ac5da145b1b41aee718853e943cf6a9
David Weber <davidjwe33@gmail.com>
2016-10-11 14:09:46 -0700

    save before merge

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -3,14 +3,27 @@ var
   passport = require('passport'),
   gamesRouter = express.Router(),
   gamesController = require('../controllers/gamesController.js'),
-  User = require('../models/Game.js')
+  Game = require('../models/Game.js'),
+  User = require('../models/User.js')
 
 /////////Garrett you can use this to create games////////
-// gamesRouter.route('/new')
+// gamesRouter.route('/games/new')
 //   .get(gamesController.new)
 //   .post(gamesController.create)
 //
 // ////////Game view///////////
 //
-// gamesRouter.route('/game/:id')
-//   .get(gamesController.show)
+gamesRouter.get('/game/:id', function(req, res){
+  console.log("hello")
+  Game.findById(req.params.id, function(err, game){
+    res.render('game-player', {game: game})
+  })
+  // Game.findById(req.params.id, function(err, game){
+  //   if(err) return console.log(err)
+  //   console.log(game)
+  //   res.json(game)
+  // })
+})
+
+
+module.exports = gamesRouter
