021b6a481d7412a295ddb3ee9c96511c52fefa27
Philip Lin <philipo227@hotmail.com>
2016-10-11 18:47:32 -0700

    update

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -6,13 +6,14 @@ module.exports = {
 //   new: function(req, res){
 //     res.render('')
 //   },
-//
+
+//photo library index
     index: function(req,res){
       Game.find({},function(err, game){
     if(err) throw err
     res.render('photo-library.ejs', {title:"photo library uploads"})
-    
-    }
+  })
+},
 
     show: function(req, res){
       Game.findById(req.params.id, function(err, game){
