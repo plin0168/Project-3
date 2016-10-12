cbcc1c0baef574e8efe9571cbad9577a1209b3b2
Garrett Estrin <garrett.estrin@gmail.com>
2016-10-12 15:46:25 -0700

    added words array

---
+++ words.js
@@ -0,0 +1,194 @@
+var words =
+  [
+  'Magenta',
+  'Hysterical',
+  'Fallacious',
+  'Taboo',
+  'Solid',
+  'Lively',
+  'Hideous',
+  'Maddening',
+  'Freezing',
+  'Obese',
+  'Ambitious',
+  'Political',
+  'Lovely',
+  'Flippant',
+  'Picayune',
+  'Well-made',
+  'Blushing',
+  'Astonishing',
+  'Stimulating',
+  'Cynical',
+  'Sulky',
+  'Silent',
+  'Voiceless',
+  'Fresh',
+  'Upset',
+  'Well-groomed',
+  'Excited',
+  'Macho',
+  'Loving',
+  'Elastic',
+  'Spicy',
+  'Drab',
+  'Noxious',
+  'Fancy',
+  'Rude',
+  'Sophisticated',
+  'Plucky',
+  'Rough',
+  'Elated',
+  'Nostalgic',
+  'Difficult',
+
+  'Uptight',
+
+  'Habitual',
+
+  'Woozy',
+
+  'Quiet',
+
+  'Thirsty',
+
+  'Fearful',
+
+  'Happy',
+
+  'Vagabond',
+
+  'Tall',
+
+  'Swanky',
+
+  'Average',
+
+  'Obscene',
+
+  'Parched',
+
+  'Uninterested',
+
+  'Important',
+
+  'Wooden',
+
+  'Late',
+
+  'Materialistic',
+
+  'Alluring',
+
+  'Square',
+
+  'Capable',
+
+  'Gruesome',
+
+  'Thick',
+
+  'Guiltless',
+
+  'Cold',
+
+  'Huge',
+
+  'Cuddly',
+
+  'Slimy',
+
+  'Perfect',
+
+  'Smart',
+
+  'Watery',
+
+  'Meaty',
+
+  'Special',
+
+  'Colorful',
+
+  'Unhealthy',
+
+  'Energetic',
+
+  'Petite',
+
+  'Romantic',
+
+  'Nauseating',
+
+  'Bad',
+
+  'Frightened',
+
+  'Humor',
+
+  'News',
+
+  'Music',
+
+  'Burn',
+
+  'Thought',
+
+  'Kiss',
+
+  'Curve',
+
+  'Pull',
+
+  'Crush',
+
+  'Blow',
+
+  'Coal',
+
+  'Operation',
+
+  'Transport',
+
+  'Authority',
+
+  'Comfort',
+
+  'Harmony',
+
+  'Society',
+
+  'Road',
+
+  'War',
+
+  'Pleasure',
+
+  'Desire',
+
+  'Approval',
+
+  'Flame',
+
+  'Plant',
+
+  'Sign',
+
+  'Sex',
+
+  'Government',
+
+  'Shame',
+
+  'Bite',
+
+  'Division',
+
+  'Fight',
+
+  'Attraction',
+
+  'Surprise',
+    ]
+
+modules.export = words
